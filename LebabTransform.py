297266872ed679359224c768dde68b859e25d559
uni <unibtc@gmail.com>
2018-04-20 00:59:31 +0800

    use traceback.print_exc, re-use view, format stderr message.

--- LebabTransform.py
+++ LebabTransform.py
@@ -11,7 +11,7 @@
 
 import sublime, sublime_plugin
 import platform
-import os, sys
+import os, sys, traceback
 from subprocess import Popen, PIPE
 
 PLUGIN_NAME = "LebabTransform"
@@ -23,48 +23,55 @@ USER_OS = platform.system()
 class LebabtransformCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         try:            
-            transforms = get_pref("transforms")
+            transforms = get_pref("transforms",self.view)
             lebab_exec = get_lebab_exec()
             command = [lebab_exec, self.view.file_name(), "-o", self.view.file_name(), "--transform", ",".join(transforms)]            
             cdir = os.path.dirname(self.view.file_name())
             p = execute(command, cdir, self.view)
         except:
             # Something bad happened.
-            msg = str(sys.exc_info()[1])
-            print("Unexpected error({0}): {1}".format(sys.exc_info()[0], msg))
-            sublime.error_message(msg)
+            # Print traceback on sublime's console
+            traceback.print_exc()
+            # Print and show the error message on sublime's error window
+            sublime.error_message(str(sys.exc_info()[1]))
 
 class LebabtransformEventListener(sublime_plugin.EventListener):
     def on_post_save_async(self, view):
-        if (get_pref("format_on_save") and isJavascript()):
+        if (get_pref("format_on_save",view) and isJavascript(view)):
             view.window().run_command("lebabtransform")
 
-def clear_status():
-    sublime.active_window().destroy_output_panel("Lebab_Warning")
+def clear_status(view):
+    view.window().destroy_output_panel("Lebab_Warning")
 
-def show_status_msg(msg):
-    if(get_pref("show_warnings")):
-        wnd = sublime.active_window()
+def show_status_msg(proc,view):
+    clear_status(view)
+    if(get_pref("show_warnings",view)):
+        msg = proc.stderr.read()
+        if(msg == ''):            
+            return
+        wnd = view.window()
         panel_view = wnd.create_output_panel("Lebab_Warning", True)
         wnd.run_command("show_panel", {"panel": "output.Lebab_Warning"})
         panel_view.set_read_only(False)
-        panel_view.run_command('insert', {'characters': msg})
+        panel_view.run_command('insert', {'characters': ''.join(msg.split('  '))})
         panel_view.set_read_only(True)
+    else:
+        print(proc.stdout.read())
 
-def get_pref(key):
+def get_pref(key,view):
     global_settings = sublime.load_settings(SETTINGS_FILE)
     value = global_settings.get(key)
     
-    project_settings = sublime.active_window().active_view().settings()
+    project_settings = view.settings()
 
     if project_settings.has(PLUGIN_NAME):
         value = project_settings.get(PLUGIN_NAME).get(key, value)
 
     return value
 
-def isJavascript():
-    name = sublime.active_window().active_view().file_name()
-    project_settings = sublime.active_window().active_view().settings()
+def isJavascript(view):
+    name = view.file_name()
+    project_settings = view.settings()
     if (name and os.path.splitext(name)[1][1:] in ["js"]):
         return True
 
@@ -83,20 +90,12 @@ def get_lebab_exec():
 
 def execute(cmd, cdir, view):
     try:
-        p = Popen(cmd,
-                  stdout=PIPE, stdin=PIPE, stderr=PIPE,
-                  cwd=cdir, shell=USER_OS == 'Windows')
-    except OSError as e:
-        raise Exception(e.output)
-    stdout, stderr = p.communicate()
-    stdout = stdout.decode('utf-8')
-    stderr = stderr.decode('utf-8')
+        with Popen(cmd,stdout=PIPE, stdin=PIPE,
+            stderr=PIPE, cwd=cdir, shell=USER_OS == 'Windows',universal_newlines=True) as proc:
+            show_status_msg(proc,view)
 
-    if stderr:
-        show_status_msg(stderr)
-        return False
-    else:
-        clear_status()
-        return stdout
+    except OSError as e:
+        raise Exception(e)
+
 
 
